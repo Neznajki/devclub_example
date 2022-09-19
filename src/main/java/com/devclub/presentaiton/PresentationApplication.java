e8e172c446c063df6fa0b0bcccb680a518f1d09d
Maris Locmelis <Maris_Locmelis@swissre.com>
2022-09-19 11:26:14 +0300

    fixed typo

--- src/main/java/com/devclub/presentaiton/PresentationApplication.java
+++ src/main/java/com/devclub/presentaiton/PresentationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PresentaitonApplication {
+public class PresentationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PresentaitonApplication.class, args);
+        SpringApplication.run(PresentationApplication.class, args);
     }
 
 }
